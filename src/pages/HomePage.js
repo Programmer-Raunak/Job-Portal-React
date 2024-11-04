ddf18c1b1dd191b2d0e50e347f1ad65427f72f2d
𝐑𝐀𝐔𝐍𝐀𝐊  ||  ᴡᴇʙ ᴅᴇᴠᴇʟᴏᴘᴇʀ <158747587+Programmer-Raunak@users.noreply.github.com>
2024-11-04 16:44:24 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -60,7 +60,7 @@ const HomePage = ({ searchParams }) => {
             'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY, 
             // Include the RapidAPI key from environment variables for API authentication.
 
-            'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST, 
+            'X-RapidAPI-Host': 'faaf03c207msh2181f51adcaead5p138be4jsn533b518463a0, 
             // Include the RapidAPI host from environment variables.
 
           },
